077c09d8cddd8971373008277e6f5bf8a446fa98
londeroner <Filin_s1999@mail.ru>
2019-09-23 22:10:07 +0300

    First UI tests

--- game_project/Source/menuOverlay.cpp
+++ game_project/Source/menuOverlay.cpp
@@ -21,8 +21,29 @@ void updateMenuOverlay(double deltaTime)
 	if (gEnv->game.debug)
 	{
 
-		sf::CircleShape f(120.f);
-		gEnv->globalWindow.draw(f);
+		//sf::CircleShape f(120.f);
+		//gEnv->globalWindow.draw(f);
+
+		tgui::Theme theme("D:/Projects/_project/TGUI/themes/Black.txt");
+		
+		auto button = tgui::Button::create();
+		button->setRenderer(theme.getRenderer("Button"));
+		button->setSize(100, 100);
+		button->setPosition("(&.size - size)/2","(&.size - size) / 2");
+		button->setText("A");
+		button->setTextSize(72);
+		gEnv->globalGui.add(button, "TestButton");
+
+		auto button2 = tgui::Button::create();
+		gEnv->globalGui.add(button2);
+		button2->setPosition("TestButton.x", "TestButton.bottom + 100");
+		button2->setRenderer(theme.getRenderer("Button"));
+		button2->setSize(100, 100);
+		button2->setText("A");
+		button2->setTextSize(72);
+		
+		
+		gEnv->globalGui.draw();
 
 	}
 
