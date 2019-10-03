6856c420f2d3438076ba5375d448eee112f62acb
londeroner <Filin_s1999@mail.ru>
2019-10-03 11:23:43 +0300

    its not working

--- game_project/Source/mainMenu.cpp
+++ game_project/Source/mainMenu.cpp
@@ -1,11 +1,18 @@
 #include "mainMenu.h"
 #include "enviroment.h"
+#include "envUtils.h"
 
 void updateMainMenu()
 {
 	if (gEnv->game.mainMenu.menuDrawRequired)
 	{
 		gEnv->globalGui.draw();
+		sf::Text text;
+		text.setFont(*gEnv->globalGui.getFont());
+		text.setString("ITS WORKS");
+		text.setPosition(200, 200);
+		text.setFillColor({ 255, 0, 0 });
+		gEnv->globalWindow.draw(text);
 	}
 
 	if (gEnv->game.mainMenu.widgetDisable)
@@ -103,7 +110,7 @@ void createMenuButtons()
 
 	//TESTS TESTS TESTS
 
-	tgui::EditBox::Ptr edb = tgui::EditBox::create();
+	/*tgui::EditBox::Ptr edb = tgui::EditBox::create();
 	gEnv->globalGui.add(edb, "editBox");
 	edb->setRenderer(gEnv->globalTheme.getRenderer("EditBox"));
 	edb->setSize(200, 50);
@@ -111,12 +118,113 @@ void createMenuButtons()
 	edb->setPosition(10, 270);
 	edb->setDefaultText("Click to edit text...");
 
-	tgui::Label::Ptr label = tgui::Label::create();
-	gEnv->globalGui.add(label, "label");
+	tgui::Label::Ptr label0 = tgui::Label::create();
+	gEnv->globalGui.add(label0, "label");
+	label0->setRenderer(gEnv->globalTheme.getRenderer("Label"));
+	label0->setText("This is a label.");
+	label0->setPosition(10, 90);
+	label0->setTextSize(18);
+
+	tgui::ListBox::Ptr lb = tgui::ListBox::create();
+	gEnv->globalGui.add(lb);
+	lb->setRenderer(gEnv->globalTheme.getRenderer("ListBox"));
+	lb->setSize(250, 120);
+	lb->setItemHeight(24);
+	lb->setPosition(10, 340);
+	lb->addItem("Item 1");
+	lb->addItem("Item 2");
+	lb->addItem("Item 3");
+
+	tgui::ProgressBar::Ptr pb = tgui::ProgressBar::create();
+	gEnv->globalGui.add(pb);
+	pb->setRenderer(gEnv->globalTheme.getRenderer("ProgressBar"));
+	pb->setPosition(10, 500);
+	pb->setSize(200, 20);
+	pb->setValue(50);
+
+	tgui::Slider::Ptr sl = tgui::Slider::create();
+	gEnv->globalGui.add(sl);
+	sl->setRenderer(gEnv->globalTheme.getRenderer("Slider"));
+	sl->setPosition(10, 560);
+	sl->setSize(200, 18);
+	sl->setValue(4);
+
+	auto scrollbar = tgui::Scrollbar::create();
+	scrollbar->setRenderer(gEnv->globalTheme.getRenderer("Scrollbar"));
+	scrollbar->setPosition(380, 40);
+	scrollbar->setSize(18, 540);
+	scrollbar->setMaximum(100);
+	scrollbar->setViewportSize(70);
+	gEnv->globalGui.add(scrollbar);
+
+	auto comboBox = tgui::ComboBox::create();
+	comboBox->setRenderer(gEnv->globalTheme.getRenderer("ComboBox"));
+	comboBox->setSize(120, 21);
+	comboBox->setPosition(420, 40);
+	comboBox->addItem("Item 1");
+	comboBox->addItem("Item 2");
+	comboBox->addItem("Item 3");
+	comboBox->setSelectedItem("Item 2");
+	gEnv->globalGui.add(comboBox);
+
+	auto child = tgui::ChildWindow::create();
+	child->setRenderer(gEnv->globalTheme.getRenderer("ChildWindow"));
+	child->setSize(250, 120);
+	child->setPosition(420, 80);
+	child->setTitle("Child window");
+	gEnv->globalGui.add(child);
+
+	auto label = tgui::Label::create();
 	label->setRenderer(gEnv->globalTheme.getRenderer("Label"));
-	label->setText("This is a label.\nAnd these are radio buttons:");
-	label->setPosition(10, 90);
-	label->setTextSize(18);
+	label->setText("Hi! I'm a child window.");
+	label->setPosition(30, 30);
+	label->setTextSize(15);
+	child->add(label);
+
+	auto button = tgui::Button::create();
+	button->setRenderer(gEnv->globalTheme.getRenderer("Button"));
+	button->setPosition(75, 70);
+	button->setText("OK");
+	button->setSize(100, 30);
+	button->connect("pressed", [=]() { child->setVisible(false); });
+	child->add(button);
+
+	auto checkbox = tgui::CheckBox::create();
+	checkbox->setRenderer(gEnv->globalTheme.getRenderer("CheckBox"));
+	checkbox->setPosition(420, 240);
+	checkbox->setText("Ok, I got it");
+	checkbox->setSize(25, 25);
+	gEnv->globalGui.add(checkbox);
+
+	auto chatbox = tgui::ChatBox::create();
+	chatbox->setRenderer(gEnv->globalTheme.getRenderer("ChatBox"));
+	chatbox->setSize(300, 100);
+	chatbox->setTextSize(18);
+	chatbox->setPosition(420, 310);
+	chatbox->setLinesStartFromTop();
+	chatbox->addLine("texus: Hey, this is TGUI!", sf::Color::Green);
+	chatbox->addLine("Me: Looks awesome! ;)", sf::Color::Yellow);
+	chatbox->addLine("texus: Thanks! :)", sf::Color::Green);
+	chatbox->addLine("Me: The widgets rock ^^", sf::Color::Yellow);
+	gEnv->globalGui.add(chatbox);
+	*/
+	sf::Sprite sprite;
+
+	sprite.setTexture(getModelDescriptor("blankTexture")->tex);
+	sprite.setScale(200.f / getModelDescriptor("blankTexture")->tex.getSize().x, 140.f / getModelDescriptor("blankTexture")->tex.getSize().y);
+
+	sf::Text text{ "SFML Canvas", *gEnv->globalGui.getFont(), 48 };
+	text.setPosition(200, 200);
+	text.setFillColor({ 255, 0, 0 });
+
+	auto canvas = tgui::Canvas::create({ 200, 140 });
+	canvas->setPosition(10, 10);
+	canvas->clear();
+	canvas->draw(sprite);
+	canvas->draw(text);
+	canvas->display();
+	gEnv->globalGui.add(canvas);
+
 
 }
 
