5966868725bfbd159de550fc39fce685ca1c405c
Daniel Iris <Daniel Iris@Dan-PC>
2019-11-15 13:26:18 +0300

    item

---
+++ game_project/Source/item.h
@@ -0,0 +1,115 @@
+#pragma once
+
+//
+// This file contain item declaration
+//
+//
+//
+
+#include <string>
+#include "baseObject.h"
+
+namespace itemType {
+	enum ItemType
+	{
+
+		null,
+
+	};
+} // itemType
+
+namespace itemModifier {
+	enum ItemModifier
+	{
+		broken,
+		bad,
+		normal,
+		upgrade1,
+		upgrade2,
+		legendary
+
+	};
+}
+
+namespace moduleType {
+	enum ModuleType 
+	{
+		null,
+	};
+}
+
+namespace equipmentType {
+	enum EquipmentType
+	{
+		null,
+	};
+}
+
+namespace moduleSlot {
+	enum ModuleSlotType
+	{
+		null,
+		core,
+		hyperdrive,
+		engine,
+		system,
+		primaryWeapon,
+		secondaryWeapon,
+		universal,
+	};
+	enum ModuleSlotSize
+	{
+		small,
+		medium,
+		large,
+		huge,
+	};
+}
+
+class Item : public BaseObject
+{
+public:
+	int level;
+	int rarity;
+	int itemId;
+	itemType::ItemType itemType;
+	std::string name;
+	Item()
+	{
+		itemType = itemType::null;
+	}
+};
+
+class ItemConstructable : public Item
+{
+public:
+	// Constructor constructor;
+	__int64 key;
+	int quality;
+	itemModifier::ItemModifier modifier;
+};
+
+class ItemEquipable : public ItemConstructable
+{
+public:
+};
+
+class Module : public ItemEquipable
+{
+public:
+	moduleType::ModuleType moduleType;
+	moduleSlot::ModuleSlotType slot;
+	moduleSlot::ModuleSlotSize size;
+};
+
+class Equipment : public ItemEquipable
+{
+public:
+	equipmentType::EquipmentType equipmentType;
+};
+
+class WeaponModule : public Module
+{
+public:
+	
+};
