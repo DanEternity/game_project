20e1ef2f8c87ce0694894318124d04dfdeff25ec
londeroner <Filin_s1999@mail.ru>
2020-04-22 16:43:51 +0300

    Jump

--- game_project/Source/map.cpp
+++ game_project/Source/map.cpp
@@ -18,5 +18,56 @@ void BuildMapUI()
 		but->setSize(20, 20);
 		panel->add(but);
 		but->setPosition(sector.second->x / 10 + (but->getParent()->getSize().x / 2), sector.second->y / 15 + (but->getParent()->getSize().y / 2));
+		but->connect("MouseReleased", clickSector, sector.first);
 	}
+}
+
+void clickSector(const std::string str, tgui::Widget::Ptr widget, const std::string& signalName)
+{
+	if (!gEnv->game.ui.rmWasClicked)
+	{
+		tgui::Panel::Ptr panel = tgui::Panel::create();
+		panel->setRenderer(gEnv->globalTheme.getRenderer("Panel"));
+		gEnv->game.adventureGUI.get<tgui::Panel>("globalMapPanel")->add(panel, "tempRightPanel");
+		panel->setPosition(widget->getPosition().x + 20, widget->getPosition().y + 20);
+		panel->setSize(120, 30);
+
+		tgui::Button::Ptr but = tgui::Button::create();
+		but->setRenderer(gEnv->globalTheme.getRenderer("Button"));
+		but->setSize(120, 30);
+		panel->add(but);
+		but->setPosition(0, 0);
+		but->setText("Jump");
+		but->connect("MouseReleased", jumpSector, str);
+		gEnv->game.ui.rmWasClicked = true;
+	}
+	else
+	{
+		gEnv->game.adventureGUI.get<tgui::Panel>("globalMapPanel")->remove(gEnv->game.adventureGUI.get<tgui::Panel>("tempRightPanel"));
+
+		tgui::Panel::Ptr panel = tgui::Panel::create();
+		panel->setRenderer(gEnv->globalTheme.getRenderer("Panel"));
+		gEnv->game.adventureGUI.get<tgui::Panel>("globalMapPanel")->add(panel, "tempRightPanel");
+		panel->setPosition(widget->getPosition().x + 20, widget->getPosition().y + 20);
+		panel->setSize(120, 30);
+
+		tgui::Button::Ptr but = tgui::Button::create();
+		but->setRenderer(gEnv->globalTheme.getRenderer("Button"));
+		but->setSize(120, 30);
+		panel->add(but);
+		but->setPosition(0, 0);
+		but->setText("Jump");
+		but->connect("MouseReleased", jumpSector, str);
+	}
+}
+
+void jumpSector(const std::string str, tgui::Widget::Ptr widget, const std::string& signalName)
+{
+	
+	gEnv->game.adventureGUI.get<tgui::Panel>("globalMapPanel")->remove(gEnv->game.adventureGUI.get<tgui::Panel>("tempRightPanel"));
+	gEnv->game.ui.rmWasClicked = false;
+
+	//Dan write code here
+	//
+	//
 }
